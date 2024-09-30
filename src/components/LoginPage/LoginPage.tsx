7e086516bf3259ffa06602b070db83b15c93ee22
yuyan2023 <971670319@qq.com>
2024-09-30 14:23:51 +1000

    map and login

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,63 +1,34 @@
 import React from 'react';
-import './loginpage.css'; // Import your specific CSS file for the LoginPage
+import { FaApple, FaGoogle, FaEnvelope } from 'react-icons/fa';
+import './LoginPage.css'; // We'll create this file for LoginPage-specific styles
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="container py-5 h-100">
-      <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div className="text-center">
-            <img src="assets/images/sobericon.png" alt="Sober Guide Icon" style={{ width: '20vh' }} />
-          </div>
-          
-          <div className="card bg-transparent" style={{ borderRadius: '1rem' }}>
-            <div className="card-body p-2 text-center">
-              <div className="mb-md-5 mt-md-4 pb-5">
-                <form action="/login" method="POST">
-                  <div className="form-outline form-white mb-4">
-                    <input 
-                      type="text" 
-                      id="typeEmailX" 
-                      name="username" 
-                      className="form-control form-control-lg" 
-                      placeholder="Username" 
-                    />
-                  </div>
-                  <div className="form-outline form-white mb-4">
-                    <input 
-                      type="password" 
-                      id="typePasswordX" 
-                      name="password" 
-                      className="form-control form-control-lg" 
-                      placeholder="Password" 
-                    />
-                  </div>
-                  <button className="btn btn-custom-green btn-lg px-5 mb-3 w-100" type="submit">
-                    Login
-                  </button>
-                </form>
-                
-                <a href="pages/register/register.html" className="btn btn-custom-green btn-lg px-5 mb-3 w-100" role="button">
-                  Register
-                </a>
-                <div>
-                  <a href="pages/register/reset.html" className="text-white-50 fw-bold">Forget password?</a>
-                </div>
-                <div className="d-flex justify-content-center text-center mt-2 pt-1">
-                  <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                    <a href="#!" className="social-link">
-                      <img src="assets/images/apple.png" alt="apple" className="social-icon" />
-                    </a>
-                    <a href="#!" className="social-link mx-3">
-                      <img src="assets/images/google.png" alt="google" className="social-icon" />
-                    </a>
-                    <a href="#!" className="social-link">
-                      <img src="assets/images/email.png" alt="email" className="social-icon" />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div className="login-container">
+      <div className="login-card">
+        <div className="logo-container">
+          <img src="/src/assets/sobericon.png" alt="Sober Guide Icon" className="logo" />
+          <h2 className="welcome-text">Welcome Back</h2>
+        </div>
+
+        <form className="login-form">
+          <input type="text" placeholder="Username" className="login-input" />
+          <input type="password" placeholder="Password" className="login-input" />
+          <button type="submit" className="login-button">Login</button>
+        </form>
+
+        <div className="login-links">
+          <a href="#" className="login-link">Register</a>
+          <span className="separator">|</span>
+          <a href="#" className="login-link">Forgot password?</a>
+        </div>
+
+        <div className="social-login">
+          <p className="social-text">Or continue with</p>
+          <div className="social-icons">
+            <a href="#" className="social-icon"><FaApple /></a>
+            <a href="#" className="social-icon"><FaGoogle /></a>
+            <a href="#" className="social-icon"><FaEnvelope /></a>
           </div>
         </div>
       </div>
