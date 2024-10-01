0a88c94d6d4b8e319a25b4efadfca5b59c0242cb
Andy Wang <95andyw@gmail.com>
2024-10-01 13:25:22 +1000

    nav_bar_2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GoogleMaps from './components/Map/Map';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/Register/RegisterPage';
-import './App.css';
-import './styles/global.css';
 import NavBar from './components/NavBar/NavBar';
+import './styles/Global.css';
+import BackButton from './components/BackButton/BackButton';
+import SGTitle from './components/SGTitle/SGTitle';
 
 function App() {
-  const apiKey = 'YOUR_GOOGLE_MAPS_API_KEY'; // Replace with your actual API key
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
   return (
     <Router>
-      <div className="App" id="mobile-view">
-        <NavBar />
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<GoogleMaps apiKey={apiKey} />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
+      <div id="mobile-view">
+        <div className="App">
+          <header className="header">
+            <BackButton />
+            <SGTitle />
+            <NavBar />
+          </header>
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<GoogleMaps apiKey={apiKey} />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+          </main>
+        </div>
       </div>
     </Router>
   );
