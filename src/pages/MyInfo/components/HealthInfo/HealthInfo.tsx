cad5e510ab73dd836a8033984938207d23e97464
Andy Wang <95andyw@gmail.com>
2024-10-12 00:35:58 +1000

    edit info page

--- src/pages/MyInfo/components/HealthInfo/HealthInfo.tsx
+++ src/pages/MyInfo/components/HealthInfo/HealthInfo.tsx
@@ -15,9 +15,12 @@ interface HealthInfoProps extends HealthData {
 const HealthInfo: React.FC<HealthInfoProps> = ({ age, height, weight, gender, onUpdate }) => {
   return (
     <div className={styles.healthContainer}>
-      <div>
       <p className={styles.title}>Health Information</p>
+      
+      <div className={styles.inputGroup}>
+        <label htmlFor="age" className={styles.labelClass}>Age:</label>
         <input
+          id="age"
           type="number"
           value={age ?? ''}
           onChange={(e) => onUpdate('age', e.target.value ? Number(e.target.value) : null)}
@@ -25,36 +28,47 @@ const HealthInfo: React.FC<HealthInfoProps> = ({ age, height, weight, gender, on
           className={styles.input}
         />
       </div>
-      <div>
+      
+      <div className={styles.inputGroup}>
+        <label htmlFor="height" className={styles.labelClass}>Height (cm):</label>
         <input
+          id="height"
           type="number"
           value={height ?? ''}
           onChange={(e) => onUpdate('height', e.target.value ? Number(e.target.value) : null)}
-          placeholder="Enter your height (cm)"
+          placeholder="Enter your height"
           className={styles.input}
         />
       </div>
-      <div>
+      
+      <div className={styles.inputGroup}>
+        <label htmlFor="weight" className={styles.labelClass}>Weight (kg):</label>
         <input
+          id="weight"
           type="number"
           value={weight ?? ''}
           onChange={(e) => onUpdate('weight', e.target.value ? Number(e.target.value) : null)}
-          placeholder="Enter your weight (kg)"
+          placeholder="Enter your weight"
           className={styles.input}
         />
       </div>
-      <div>
-        <input
-          type="text"
+      
+      <div className={styles.inputGroup}>
+        <label htmlFor="gender" className={styles.labelClass}>Gender:</label>
+        <select
+          id="gender"
           value={gender}
           onChange={(e) => onUpdate('gender', e.target.value)}
-          placeholder="Enter your gender"
           className={styles.input}
-        />
+        >
+          <option value="">Select Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Other">Other</option>
+          <option value="Prefer not to say">Prefer not to say</option>
+        </select>
       </div>
     </div>
-    
-    
   );
 };
 
