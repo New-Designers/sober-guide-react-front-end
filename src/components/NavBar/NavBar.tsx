3a56411b09316f8558e8021f62c590f1487ea0d2
Andy Wang <95andyw@gmail.com>
2024-10-15 21:40:10 +1000

    edit nav

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -85,7 +85,7 @@ const NavBar: React.FC = () => {
     { text: 'Profile', icon: <MedicalInformationOutlinedIcon />, path: '/profile' },
     { text: 'My Info', icon: <LocalHospitalIcon />, path: '/my-info' },
     { text: 'Map', icon: <MapIcon />, path: '/map' },
-    { text: 'Drinking Plan', icon: <LocalBarIcon />, path: '/drinking-plan' },
+    { text: 'Plan', icon: <LocalBarIcon />, path: '/drinking-plan' },
     { text: 'Rewards', icon: <EmojiEventsIcon />, path: '/reward' },
     { text: 'Tips', icon: <TipsAndUpdatesIcon />, path: '/tips' },
     { text: 'Records', icon: <TourIcon />, path: '/time-tracking' },
